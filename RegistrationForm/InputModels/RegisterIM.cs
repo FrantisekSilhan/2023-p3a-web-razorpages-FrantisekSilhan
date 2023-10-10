ec9b4c6cf1a0cb0f54eb1e443913631fae4fb0f2
František Šilhán <frantiseksilhann@gmail.com>
2023-10-10 10:04:16 +0200

    error message

--- RegistrationForm/InputModels/RegisterIM.cs
+++ RegistrationForm/InputModels/RegisterIM.cs
@@ -6,26 +6,26 @@ namespace RegistrationForm.InputModels
     public class RegisterIM
     {
         [Display(Name = "Jméno")]
-        [Required]
+        [Required(ErrorMessage = "Jméno musí být zadáno.")]
         public string FirstName {  get; set; }
         [Display(Name = "Příjmení")]
-        [Required]
+        [Required(ErrorMessage = "Příjmení musí být zadáno.")]
         public string LastName { get; set; }
         [Display(Name = "Datum narození")]
         public DateTime BirthDate { get; set; }
         [Display(Name = "Název školy")]
         public string SchoolName { get; set; }
-        [Required]
         [Display(Name = "Ročník")]
+        [Required(ErrorMessage = "Ročník musí být zadán.")]
         public SchoolGrade Grade { get; set; }
         [Display(Name = "Zájemce o studium")]
         public bool Aspirant { get; set; }
-        [Required]
-        [EmailAddress]
         [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email musí být zadán.")]
+        [EmailAddress]
         public string Email { get; set; }
-        [Required]
         [Display(Name = "Heslo")]
+        [Required(ErrorMessage = "Heslo musí být zadáno.")]
         public string Password { get; set; }
     }
 }
